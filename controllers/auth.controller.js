5c27a025f2edd90f53b32d041082279c4eadad5b
KuraoHikari <dewaindra705@gmail.com>
2023-06-18 20:23:34 +0800

    [+] bcrypt, passport , [U] auth.controller, gitignore  [T] login, register ejs Update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,16 +1,68 @@
+const {
+ PrismaClient,
+ Prisma,
+} = require("@prisma/client");
+const prisma = new PrismaClient();
+const {
+ generateHash,
+} = require("../utils/bcrypt");
+const passport = require("../utils/passport");
+
 class AuthController {
  static loginPage(req, res) {
+  if (req.isAuthenticated())
+   return res.redirect("/message");
+
   res.render("pages/auth/login");
  }
  static registerPage(req, res) {
   res.render("pages/auth/register");
  }
 
- static register(req, res) {
-  res.redirect("/login");
+ static async register(req, res) {
+  try {
+   const { email, password } = req.body;
+
+   await prisma.user.create({
+    data: {
+     email: email,
+     password: await generateHash(
+      password
+     ),
+    },
+   });
+
+   res.redirect("/login");
+  } catch (error) {
+   if (
+    err instanceof
+    Prisma.PrismaClientKnownRequestError
+   ) {
+    if (err.code === "P2002") {
+     req.flash(
+      "error",
+      "A category with this name is already in use"
+     );
+    }
+   }
+   res.reder("/register");
+  }
  }
  static login(req, res) {
-  res.render("pages/auth/register");
+  passport.authenticate("local", {
+   successRedirect: "/message",
+   failureRedirect: "/login",
+   failureFlash: true,
+  })(req, res, next);
+ }
+ static async logout(req, res, next) {
+  req.logout((err) => {
+   if (err) {
+    return next(err);
+   }
+
+   res.redirect("/login");
+  });
  }
 }
 module.exports = AuthController;
---
+++ utils/bcrypt.js
@@ -0,0 +1,22 @@
+const {
+ hash,
+ compare,
+} = require("bcrypt");
+
+const salt = 10;
+
+async function generateHash(password) {
+ return await hash(password, salt);
+}
+
+async function compareHash(
+ password,
+ hash
+) {
+ return await compare(password, hash);
+}
+
+module.exports = {
+ generateHash,
+ compareHash,
+};
